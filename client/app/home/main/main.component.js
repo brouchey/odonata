6b0536a89df94881e8a4046cbfd1394e86c8c5db
brouchey <benrouch@me.com>
2017-03-08 18:40:41 +0100

    main and portal minor design changes and testing

--- client/app/home/main/main.component.js
+++ client/app/home/main/main.component.js
@@ -1,52 +1,64 @@
 'use strict';
 const angular = require('angular');
 
+//- from SignupController
+type User = {
+	name: string;
+	email: string;
+	password: string;
+};
+
 export class MainComponent {
 
-	//- from LoginController
+	//- from SignupController
 	user: User = {
-    name: '',
-    email: '',
-    password: ''
-  };
-  errors = {
-    login: undefined
-  };
-  submitted = false;
-  Auth;
-  $location;
+		name: '',
+		email: '',
+		password: ''
+	};
+	errors = {};
+	submitted = false;
+	Auth;
+	$location;
 
-  /*@ngInject*/
-  constructor(Auth, $location) {
-  	this.Auth = Auth;
-    this.$location = $location;
-  }
+	/*@ngInject*/
+	constructor(Auth, $location) {
+		this.Auth = Auth;
+		this.$location = $location;
+	}
 
-  //- from LoginController
-  login(form) {
-    this.submitted = true;
+	//- from SignupController
+	register(form) {
+		this.submitted = true;
 
-    if(form.$valid) {
-      this.Auth.login({
-        email: this.user.email,
-        password: this.user.password
-      })
-        .then(() => {
-          // Logged in, redirect to home (portal)
-          this.$location.path('/');
-        })
-        .catch(err => {
-          this.errors.login = err.message;
-        });
-    }
-  }
+		if(form.$valid) {
+			return this.Auth.createUser({
+				name: this.user.name,
+				email: this.user.email,
+				password: this.user.password
+			})
+				.then(() => {
+					// Account created, redirect to home
+					this.$location.path('/');
+				})
+				.catch(err => {
+					err = err.data;
+					this.errors = {};
+					// Update validity of form fields that match the mongoose errors
+					angular.forEach(err.errors, (error, field) => {
+						form[field].$setValidity('mongoose', false);
+						this.errors[field] = error.message;
+					});
+				});
+		}
+	}
 
 }
 
 export default angular.module('odonataApp.main', [])
-  .component('main', {
-    template: require('./main.pug'),
-    controller: MainComponent,
-    controllerAs: 'vm'
-  })
-  .name;
+	.component('main', {
+		template: require('./main.pug'),
+		controller: MainComponent,
+		controllerAs: 'vm'
+	})
+	.name;
