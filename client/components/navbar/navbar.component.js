62b3720afb622b919239bce1221b8530f8915832
brouchey <benrouch@me.com>
2017-03-13 12:50:45 +0100

    replace navbar signup and login links with a login form

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -15,6 +15,10 @@ export class NavbarComponent {
   getCurrentUser: Function;
   isCollapsed = true;
 
+  //- for Login
+  submitted = false;
+  Auth;
+
   constructor($location, Auth) {
     'ngInject';
 
@@ -22,11 +26,33 @@ export class NavbarComponent {
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
+
+    //- for Login
+    this.Auth = Auth;
   }
 
   isActive(route) {
     // return route === this.$location.path();
     return this.$location.path().indexOf(route) == 0;
+  }
+
+  //- for Login
+  login(form) {
+    this.submitted = true;
+
+    if(form.$valid) {
+      this.Auth.login({
+        email: this.user.email,
+        password: this.user.password
+      })
+        .then(() => {
+          // Logged in, redirect to home
+          this.$location.path('/');
+        })
+        .catch(err => {
+          this.errors.login = err.message;
+        });
+    }
   }
 
   hasAvatar() {
